2c11a20cfa6dcc605db0c2f1b39b24f37e7dc8c3
null <null>
2025-09-05 14:46:16 -0500

    m

--- game.js
+++ game.js
@@ -569,11 +569,11 @@ class Game3D {
     this.playerX += (this.targetX - this.playerX) * 0.1;
     this.player.position.x = this.playerX;
 
-    // Keep camera following player at fixed relative position
+    // Keep camera in fixed position behind stationary player
     this.camera.position.x = this.playerX * 0.3;
     this.camera.position.y = 15;
-    this.camera.position.z = this.player.position.z + 20;
-    this.camera.lookAt(this.player.position.x, 0, this.player.position.z);
+    this.camera.position.z = 20; // Fixed position behind player
+    this.camera.lookAt(this.playerX, 0, 0); // Always look at player position
 
     // Update distance and difficulty
     this.distance += this.speed * 100;
